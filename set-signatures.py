351e4b70644369c9c6f502e40ed4c4fa16e8142c
Erik Berkun-Drevnig <e.berkun-drevnig@neuronicworks.com>
2016-10-24 16:24:44 -0400

    Fixed script, permissions still bad

--- set-signatures.py
+++ set-signatures.py
@@ -12,21 +12,27 @@ scopes = [
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keyfile.json', scopes)
 
-def change_signature(name, email, position):
+def change_signature(email, name, position):
     delegated_credentials = credentials.create_delegated(email)
     http_auth = delegated_credentials.authorize(Http())
-
     gmail = build('gmail', 'v1', http=http_auth)
-    results = gmail.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    send_as_body = {
+        'signature': Renderer().render_path('template.mustache', {
+            'name': name,
+            'position': position
+        })
+    }
 
-    signature = Renderer().render_path('template.mustache', {
-        'name': name,
-        'position': position
-    })
+    result = gmail.users().settings().sendAs().update(
+        userId='me',
+        sendAsEmail=email,
+        body=send_as_body
+    ).execute()
 
-def main:
-    with open('users.csv', 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            change_signature(row[0], row[1], row[2])
+    print result
+
+with open('users.csv', 'rb') as f:
+    reader = csv.reader(f)
+    next(reader, None)
+    for row in reader:
+        change_signature(row[0], row[1], row[2])
