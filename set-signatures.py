09d9a9dc3736f22c9af47a1103b0da88e4aa467d
Erik Berkun-Drevnig <erik.b22@gmail.com>
2016-10-24 20:32:44 -0400

    Put example file in its place

--- set-signatures.py
+++ set-signatures.py
@@ -5,31 +5,27 @@ from oauth2client.service_account import ServiceAccountCredentials
 from apiclient.discovery import build
 from pystache import Renderer
 
-scopes = [
-    'https://mail.google.com/'
-    'https://www.googleapis.com/auth/gmail.readonly',
-]
-
+scopes = ['https://www.googleapis.com/auth/gmail.settings.basic']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keyfile.json', scopes)
 
-def change_signature(email, name, position):
+def change_signature(email, name, title):
     delegated_credentials = credentials.create_delegated(email)
     http_auth = delegated_credentials.authorize(Http())
     gmail = build('gmail', 'v1', http=http_auth)
     send_as_body = {
         'signature': Renderer().render_path('template.mustache', {
             'name': name,
-            'position': position
+            'title': title
         })
     }
+
+    print 'Changing signature of %s' % email
 
     result = gmail.users().settings().sendAs().update(
         userId='me',
         sendAsEmail=email,
         body=send_as_body
     ).execute()
-
-    print result
 
 with open('users.csv', 'rb') as f:
     reader = csv.reader(f)
